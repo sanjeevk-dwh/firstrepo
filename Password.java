269be1a905481ab7b30bc9e997582369c401ee3a
vish <bishwajeet.kumar2k10@gmail.com>
2019-05-09 18:41:31 +0530

    Payment and Passowrd file is added

---
+++ Password.java
@@ -0,0 +1,10 @@
+import java.util.*
+public Class Password
+{
+public static void main(String args[])
+{
+int a = 10;
+int b = 20;
+System.out.println(a);
+}
+}
---
+++ Payment.java
@@ -0,0 +1,10 @@
+import math
+public class Payment
+{
+public static void main (String args[])
+{
+int a = 10;
+int b = 50;
+System.out.println(a);
+}
+}
