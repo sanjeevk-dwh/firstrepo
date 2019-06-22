2040d698d991945c4052b90324ddb5fce2f2e108
Ubuntu <ubuntu@ip-172-31-84-85.ec2.internal>
2019-06-22 03:05:19 +0000

    CHanges has been done in Hi.java file from local

--- Hi.java
+++ Hi.java
@@ -1,3 +1,5 @@
 I have added new file to remote repo.
 It will help me to try pull and fetch.
 I am checking if pull overrite my local content or replace the contents.
+
+date 06/22/2019 changes added
